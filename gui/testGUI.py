629a35a9fbff7982f419425879167b784de9eb40
langshiquan <langshiquan@baidu.com>
2017-09-09 17:00:23 +0800

    commit:half_gui

---
+++ gui/testGUI.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# _*_ coding:UTF-8 _*_
+
+from Tkinter import *
+
+root = Tk();
+
+root.mainloop();
---
+++ push.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "commit:"$1
+git push origin master
